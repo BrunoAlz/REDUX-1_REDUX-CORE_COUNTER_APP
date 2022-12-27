08a570eb28ac1cbfc1fb017cc981aa0f244c8373
Bruno Alves <bruno6821@gmail.com>
2022-12-27 13:16:25 -0300

    Criando as Actions

--- counter.js
+++ counter.js
@@ -6,7 +6,25 @@ const initialState = {
   count: 0,
 };
 
-// ACTIONS:
+// ACTIONS / ACTION / ACTION CREATOR:
+// INCREMENT
+// DECREMENT
+// RESET
+// INCREMENT BY AMOUNT
+
+// ACTION
+{
+  type: "INCREMENT";
+}
+{
+  type: "DECREMENT";
+}
+{
+  type: "RESET";
+}
+{
+  type: "INCREMENT_BY_AMOUNT";
+}
 
 // REDUCERS:
 
